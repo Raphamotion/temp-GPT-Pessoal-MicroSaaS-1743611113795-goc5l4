6d3dc188b1234f7cc8d3ed8ed56832b3c2b8580c
Raphamotion <raphael.motion@gmail.com>
2025-04-02 13:25:43 -0300

    Copy supabase/functions/_shared/cors.ts from GPT-Pessoal-MicroSaaS

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,6 @@
+export const corsHeaders = {
+  // Define explicitamente a origem permitida
+  'Access-Control-Allow-Origin': 'https://k03e2io1y3fx9wvj0vr8.container-api.io',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type', // Headers que sua aplicação envia
+  'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS', // Métodos que suas funções usam
+};
